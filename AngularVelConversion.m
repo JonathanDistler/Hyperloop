1c4372f11da904148debf5ff27f45f1908e0b1e8
JonathanDistler <149265757+JonathanDistler@users.noreply.github.com>
2025-09-27 10:30:30 -0400

    Update AngularVelConversion.m
    Updates to calculate MoI and uses N2-law in angular format to determine torque produced by braking method

--- AngularVelConversion.m
+++ AngularVelConversion.m
@@ -14,13 +14,32 @@ radius = 14;
 
 % Angular velocity (rad/s)
 ang_vel = lin_vel ./ radius; 
-%Angular velocity (rad/s) rate of change
+%Angular velocity (rad/s) rate of change (rad/s^2)
 ang_vel_diff = diff(ang_vel);    
 
 %first order line of best fit between time and linear velocity
 coefficients = polyfit(elapsed_time, lin_vel, 1); % Change in angular velocity
 slope=coefficients(1)
 
-M=5; %mass in kg, measured anecodtally [NEED ACTUAL VALUE]
-MoI=(1/2)*M*radius^2;
+%%%%%%%%%%%%%%%%%Setup for MoI Calculations
+Ri=3*1/2; %Radius of inner hole in inches
+Ro=12+Ri; %Radius of outer radius including hole, in inches
+Ri_m=convlength(Ri,"in","m");
+Ro_m=convlength(Ro,"in","m");
+Depth=convlength(.3,"in","m");
+V=pi*(Ro_m^2-Ri_m^2)*Depth;
+Rho=2699; %Kg/m^3, density of Aluminum
+M=V*Rho; %Mass in Kg, determined from material and dimensions
 
+MoI=calculate(M,Ro_m,Ri_m);
+function MoI = calculate(M,Ro,Ri)
+    
+    MoI_Outer=1/2*(M)*Ro^2;
+    MoI_Inner=-1/2*(M)*Ri^2;
+    MoI=MoI_Outer-MoI_Inner;
+end
+
+%I*Theta-dd=T
+Theta_dd=mean(ang_vel_diff); %average angular acceleration
+I=MoI;
+T=Theta_dd*I; %Torque in Nm
